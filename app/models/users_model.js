3a2471f39f1bf3452dab8bd766002f3a2e5dd696
JoseLucasapp <josluc2016@gmail.com>
2020-12-14 21:46:16 -0300

    update models

--- app/models/users_model.js
+++ app/models/users_model.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 
 function users_model(){
-    mongoose.connect('mongodb://localhost:27017/teste', {useNewUrlParser: true}).then(()=>{console.log('Conectado no Mongo')}).catch((err)=>{console.log(err)});
+    mongoose.connect('mongodb://localhost:27017/teste', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{console.log('Conectado no Mongo')}).catch((err)=>{console.log(err)});
 }
 
 users_model.prototype.newUser = (req,res)=>{
-    const users = mongoose.model('userData', {name:String});
+    const users = mongoose.model('userData', {
+        name:{
+            type: String
+        },
+        age:{
+            type:Number
+        },
+        date:{
+            type: Date,
+            default: Date.now
+        }
+    });
 
     const addNewUser = new users({
-        name: req.body,
+        name: req.body.name,
     });
     addNewUser.save().then(()=>{
-        console.log('Cadastrado');
-    })
+        res.json(users);
+    }).catch((err)=>{
+        res.json(err);
+    });
 }
 
 module.exports = ()=>{
