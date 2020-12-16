d7e7128042988af47ac66a83afbd8e4b9e2374c2
JoseLucasapp <josluc2016@gmail.com>
2020-12-16 11:20:02 -0300

    add and implement cors

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const consign = require('consign');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 consign()
 .include('app/routes')
